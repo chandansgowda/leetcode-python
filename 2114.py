581340b9c692476d614bdd06f7075bebc967c415
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2023-01-16 18:00:34 +0530

    Update 2114.py

--- 2114.py
+++ 2114.py
@@ -11,3 +11,17 @@ class Solution(object):
                 mx = nw
 
         return mx
+    
+    
+    
+class Solution(object):
+    def mostWordsFound(self, sentences):
+        """
+        :type sentences: List[str]
+        :rtype: int
+        """
+        m = 0
+        for sentence in sentences:
+            m = max(m,sentence.count(" ")+1)
+
+        return m
