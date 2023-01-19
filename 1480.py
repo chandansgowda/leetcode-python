4223c12996c7826de573f24948fdd7c98d400277
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2023-01-19 21:32:12 +0530

    Create 1480.py

---
+++ 1480.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def runningSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        for i in range(1,len(nums)):
+            nums[i]+=nums[i-1]
+
+        return nums
