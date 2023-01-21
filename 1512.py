9ec130716de6df53a894408da86816798b59316c
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2023-01-21 21:31:47 +0530

    Update 1512.py

--- 1512.py
+++ 1512.py
@@ -1,3 +1,28 @@
+# Hashmap Approach
+class Solution(object):
+    def numIdenticalPairs(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        memory = {}
+        count = 0
+
+        for num in nums:
+            if num in memory:
+                if memory[num]==1:
+                    count+=1
+                else:
+                    count+=memory[num]
+                memory[num]+=1
+            else:
+                memory[num]=1
+
+        return count
+
+
+
+# Bruteforce approach
 class Solution(object):
     def numIdenticalPairs(self, nums):
         """
