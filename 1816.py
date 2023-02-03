2772ab61d5c625c207f9be74ef35fc584b98aecb
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2023-02-03 18:28:23 +0530

    Create 1816.py

---
+++ 1816.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def truncateSentence(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: str
+        """
+        return " ".join(s.split()[:k])
