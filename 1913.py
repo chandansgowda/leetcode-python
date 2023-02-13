d7d6ea8608f82a27efce0cc06c899a9515b91cc3
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2023-02-13 20:49:26 +0530

    Create 1913.py

---
+++ 1913.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def maxProductDifference(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        return (nums[-1]*nums[-2])-(nums[0]*nums[1])
