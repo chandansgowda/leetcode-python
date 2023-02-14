58fa1afe942c02c28ca68692d3c4d061e08211ef
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2023-02-14 14:52:53 +0530

    Update 005.py

--- 005.py
+++ 005.py
@@ -1,26 +1,33 @@
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution(object):
-    def addTwoNumbers(self, l1, l2):
+    def longestPalindrome(self, s):
         """
-        :type l1: ListNode
-        :type l2: ListNode
-        :rtype: ListNode
+        :type s: str
+        :rtype: str
         """
-        carry = 0
-        head = t = ListNode(0)
-        while l1 or l2 or carry:
-            if l1:
-                carry+=l1.val
-                l1 = l1.next
-            if l2:
-                carry+=l2.val
-                l2 = l2.next
-            carry, val = divmod(carry, 10)
-            t.next = ListNode(val)
-            t = t.next
+        start = 0
+        end = 0
+        m = 0
 
-        return head.next
+        for i in range(len(s)):
+
+            #odd palindromes
+            l,r=i,i
+            while l>=0 and r<len(s) and s[l]==s[r]:
+                if (r-l+1)>m:
+                    start = l
+                    end = r
+                    m = r-l+1
+                l-=1
+                r+=1
+
+            #even palindromes
+            l,r=i,i+1
+            while l>=0 and r<len(s) and s[l]==s[r]:
+                if (r-l+1)>m:
+                    start = l
+                    end = r
+                    m = r-l+1
+                l-=1
+                r+=1
+
+        return s[start:end+1]
