9fa29962551d5df5a3c4679babbb1e3954ddf855
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2023-02-19 19:37:19 +0530

    Update 989.py

--- 989.py
+++ 989.py
@@ -15,3 +15,22 @@ class Solution:
             
 
         return num
+    
+    
+class Solution:
+    def addToArrayForm(self, num: List[int], k: int) -> List[int]:
+        i = len(num)-1
+        num[i] += k
+
+        while i>0:
+            if num[i]>9:
+                c,num[i] = divmod(num[i],10)
+                num[i-1]+=c
+            i-=1
+
+        while num[i]>9:
+            c,num[i] = divmod(num[i],10)
+            num.insert(0,c)
+            
+
+        return num
