735698f1f6e012ca91d66ad78c72c1576a7f170a
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2023-02-20 21:39:33 +0530

    Create 122.py

---
+++ 122.py
@@ -0,0 +1,7 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        mp = 0
+        for i in range(len(prices)-1):
+            if prices[i+1]>prices[i]:
+                mp += prices[i+1]-prices[i]
+        return mp
