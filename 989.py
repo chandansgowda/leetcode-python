e254e8d197802b553dbac064f768945529c4d74e
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2023-02-20 21:31:18 +0530

    Update 989.py

--- 989.py
+++ 989.py
@@ -3,18 +3,18 @@ class Solution:
         i = len(num)-1
         num[i] += k
 
-        while i>0:
-            if num[i]>9:
-                c,num[i] = divmod(num[i],10)
-                num[i-1]+=c
-            i-=1
-
-        while num[i]>9:
+        while i>0 or num[i]>9:
             c,num[i] = divmod(num[i],10)
-            num.insert(0,c)
-            
-
+            if i>0:
+                num[i-1]+=c
+                i -= 1
+            else:
+                num.insert(0,c)
+                i =0
+           
         return num
+    
+    
     
     
 class Solution:
