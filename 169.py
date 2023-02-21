91d5071e8d1cd249dda0122ea0200508b5c1c668
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2023-02-21 14:59:21 +0530

    Update 169.py

--- 169.py
+++ 169.py
@@ -1,3 +1,18 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        n2 = len(nums)//2
+        d = {}
+        for i in nums:
+            if i in d:
+                d[i]+=1
+            else:
+                d[i]=1
+        for i,j in d.items():
+            if j>n2:
+                return i
+
+
+
 from collections import defaultdict
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
@@ -8,3 +23,5 @@ class Solution:
         for i,j in d.items():
             if j>n2:
                 return i
+
+            
