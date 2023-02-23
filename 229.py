4c88d8c0d466ea4826a177b523b4ccfa6a38dd20
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2023-02-23 15:25:59 +0530

    Update 229.py

--- 229.py
+++ 229.py
@@ -11,3 +11,19 @@ class Solution:
                 res.append(i)
 
         return res
+    
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        d = {}
+        n = len(nums)//3
+        res = []
+        for num in nums:
+            if num not in d:
+                d[num]=1
+            else:
+                d[num]+=1
+        for i,j in d.items():
+            if j>n:
+                res.append(i)
+
+        return res
