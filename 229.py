0cbf1ada0b59cea9f93b33d967d77603476ed4ce
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2023-02-24 20:35:18 +0530

    Update 229.py

--- 229.py
+++ 229.py
@@ -1,3 +1,23 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        c1,c2,cd1,cd2=0,0,0,1
+        for num in nums:
+            if num==cd1:
+                c1+=1
+            elif num==cd2:
+                c2+=1
+            elif c1==0:
+                c1,cd1=1,num
+            elif c2==0:
+                c2,cd2=1,num
+            else:
+                c1-=1
+                c2-=1
+        if cd1==cd2:
+            return [cd1]
+        return [n for n in (cd1,cd2) if nums.count(n)>len(nums)//3]
+
+
 from collections import defaultdict
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
@@ -11,6 +31,7 @@ class Solution:
                 res.append(i)
 
         return res
+    
     
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
