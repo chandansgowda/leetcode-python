29ab064c4d191858bb7dff91ad316b409633a5d9
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2023-02-25 21:04:49 +0530

    Update 019.py

--- 019.py
+++ 019.py
@@ -24,3 +24,22 @@ class Solution:
         slow.next = slow.next.next
         
         return temp.next
+    
+    
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        slow = fast = head
+        for _ in range(n):
+            fast = fast.next
+        if not fast:
+            return head.next
+        while fast.next:
+            fast = fast.next
+            slow = slow.next
+        slow.next = slow.next.next
+        return head
