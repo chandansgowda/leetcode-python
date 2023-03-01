53e8dfb5e236df5205a31b07fd9133c3fe0ea922
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2023-03-01 19:41:36 +0530

    Update 049.py

--- 001-500/049.py
+++ 001-500/049.py
@@ -5,3 +5,15 @@ class Solution:
         for s in strs:
             ans[tuple(sorted(s))].append(s)
         return list(ans.values())
+
+    
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        ans = {}
+
+        for s in strs:
+            if tuple(sorted(s)) in ans:
+                ans[tuple(sorted(s))].append(s)
+            else:
+                ans[tuple(sorted(s))] = [s]
+        return list(ans.values())
