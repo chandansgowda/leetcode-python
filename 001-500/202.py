f2403a7cae2688e3edf858a59985117c3e03b10c
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2023-03-03 19:23:10 +0530

    Update 202.py

--- 001-500/202.py
+++ 001-500/202.py
@@ -1,6 +1,18 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
         seen = set()
+        while n!=1:
+            n = sum(int(x)**2 for x in str(n))
+            if n in seen:
+                return False
+            seen.add(n)
+            
+        return True
+
+
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        seen = set()
         while n not in seen:
             seen.add(n)
             n = sum(int(x)**2 for x in str(n))
