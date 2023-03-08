95d46eacb4c274ea6ef266a91bad183e5a0de96b
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2023-03-08 20:38:23 +0530

    Update 258.py

--- 001-500/258.py
+++ 001-500/258.py
@@ -1,5 +1,17 @@
 class Solution:
     def addDigits(self, num: int) -> int:
+        while len(str(num))!=1:
+            res = 0
+            while num!=0:
+                num,c=divmod(num,10)
+                res+=c
+            num = res
+        return num
+
+    
+    
+class Solution:
+    def addDigits(self, num: int) -> int:
         s = str(num)
         while len(s)!=1:
             res = 0
