48710e54e61f6672c202819dd475458d36951c39
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2023-03-11 19:42:18 +0530

    Create 141.py

---
+++ 001-500/141.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        if not head or not head.next: 
+            return False
+        s = head
+        f = head.next
+
+        if s==f:
+                return True
+
+        while s is not f and f and f.next and f.next.next:
+            s = s.next
+            f = f.next.next
+            if s==f:
+                return True
+
+        return False
+
+        
