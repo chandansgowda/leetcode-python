81c2545429c3e108ca2fb567cd6f656dd10fbc63
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2023-03-11 19:41:10 +0530

    Update 2413.py

--- 2001-2500/2413.py
+++ 2001-2500/2413.py
@@ -3,3 +3,13 @@ class Solution:
         if n%2==0:
             return n
         return n*2
+    
+    
+class Solution:
+    def smallestEvenMultiple(self, n: int) -> int:
+        return n << (n & 1)
+    
+ 
+class Solution:
+    def smallestEvenMultiple(self, n: int) -> int:
+        return n*2 if n%2!=0 else n
