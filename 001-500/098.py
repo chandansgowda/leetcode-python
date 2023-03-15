a77800b6b68afbc97f0a1027e91baf2f7cb0b982
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2023-03-15 21:24:56 +0530

    Create 098.py

---
+++ 001-500/098.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+
+        def inorder(root):
+            return inorder(root.left) + [root.val] + inorder(root.right) if root else []
+
+        arr = inorder(root)
+
+        for i in range(1,len(arr)):
+            if arr[i-1]>=arr[i]:
+                return False
+
+        return True
+
+        
