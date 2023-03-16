1cec12108034d14fd98a2834a9ebdfdd1caabfe1
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2023-03-16 20:44:30 +0530

    Update 2160.py

--- 2001-2500/2160.py
+++ 2001-2500/2160.py
@@ -2,3 +2,9 @@ class Solution:
     def minimumSum(self, num: int) -> int:
         n = sorted(str(num))
         return int(n[0] + n[2]) + int(n[1] + n[3])
+
+    
+class Solution:
+    def minimumSum(self, num: int) -> int:
+        a,b,c,d = sorted(str(num))
+        return int(a+c) + int(b+d)
