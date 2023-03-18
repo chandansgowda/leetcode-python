4bb804c9e373ca6d1f8bc97d974dfd05ee5989b3
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2023-03-18 20:40:03 +0530

    Update 217.py

--- 001-500/217.py
+++ 001-500/217.py
@@ -5,3 +5,7 @@ class Solution:
             if nums[i]==nums[i+1]:
                 return True
         return False
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        return sorted(nums)!=sorted(list(set(nums)))
