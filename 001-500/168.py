bbe864588b7cc32a56be6cae3d47ba156f570c5b
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2023-03-26 20:26:17 +0530

    Update 168.py

--- 001-500/168.py
+++ 001-500/168.py
@@ -1,5 +1,18 @@
 class Solution:
     def convertToTitle(self, c: int) -> str:
+        a = [chr(x) for x in range(ord("A"), ord("Z")+2)]
+        res = ""
+        while c>0:
+          c -= 1
+          res += a[c%26]
+          c //= 26
+
+        return res[::-1]
+    
+
+
+class Solution:
+    def convertToTitle(self, c: int) -> str:
         a = ['0','A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
         res = ""
         while c>0:
