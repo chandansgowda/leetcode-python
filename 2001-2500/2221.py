1e2b0f53f41fa891a9f990c246c6047402a1db4a
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2023-04-05 20:06:34 +0530

    Create 2221.py

---
+++ 2001-2500/2221.py
@@ -0,0 +1,9 @@
+from itertools import accumulate
+class Solution:
+    def triangularSum(self, nums: List[int]) -> int:
+        n = len(nums)
+        while n>0:
+            for i in range(n-1):
+                nums[i] = (nums[i] + nums[i+1]) % 10
+            n -= 1
+        return nums[0]
