6b686380c719e0626f20d97103116d4ad6a37db5
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2023-04-08 22:15:01 +0530

    Update 1108.py

--- 1001-1500/1108.py
+++ 1001-1500/1108.py
@@ -1,5 +1,16 @@
 class Solution:
     def defangIPaddr(self, address: str) -> str:
+        res = ""
+        for i in address:
+            if i==".":
+                res += "[.]"
+            else:
+                res += i
+        return res
+
+
+class Solution:
+    def defangIPaddr(self, address: str) -> str:
         return "[.]".join(address.split("."))
 
 
