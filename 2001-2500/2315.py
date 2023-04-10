2e89831f3382c8724b8d3e38b03ff7350898b3ba
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2023-04-10 20:31:40 +0530

    Update 2315.py

--- 2001-2500/2315.py
+++ 2001-2500/2315.py
@@ -1,5 +1,18 @@
 class Solution:
     def countAsterisks(self, s: str) -> int:
+        res = 0
+        nt = 0
+        for c in s:
+            if c=="|":
+                nt+=1
+            elif nt%2==0:
+                res += c=="*"
+        return res
+
+
+
+class Solution:
+    def countAsterisks(self, s: str) -> int:
         l = s.split("|")
         res = 0
         for a in l[0::2]:
