44ed6dcf9cc8103866bf29bd14116e773e47c474
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2023-04-16 17:49:36 +0530

    Update 349.py

--- 001-500/349.py
+++ 001-500/349.py
@@ -1,3 +1,8 @@
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
         return list(set(nums1) & set(nums2))
+
+    
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        return set(nums1).intersection(nums2)
