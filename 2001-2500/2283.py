eb2f2ddfd566f5d418a620717107059f4934ee97
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2023-04-20 19:46:58 +0530

    Update 2283.py

--- 2001-2500/2283.py
+++ 2001-2500/2283.py
@@ -13,3 +13,11 @@ class Solution:
                 return False
         
         return True
+    
+    
+class Solution:
+    def digitCount(self, num: str) -> bool:
+        for i in range(len(num)):
+            if num.count(str(i))!=int(num[i]):
+                return False
+        return True
