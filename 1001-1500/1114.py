dfb030e12b0a993d0247a3fadfbf8e71e22889a4
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2023-04-23 14:35:58 +0530

    Update 1114.py

--- 1001-1500/1114.py
+++ 1001-1500/1114.py
@@ -24,3 +24,31 @@ class Foo:
             continue
         # printThird() outputs "third". Do not change or remove this line.
         printThird()
+
+        
+        
+from threading import Barrier
+
+class Foo:
+    def __init__(self):
+        self.first_barrier = Barrier(2)
+        self.second_barrier = Barrier(2)
+
+
+    def first(self, printFirst: 'Callable[[], None]') -> None:
+        # printFirst() outputs "first". Do not change or remove this line.
+        printFirst()
+        self.first_barrier.wait()
+
+
+    def second(self, printSecond: 'Callable[[], None]') -> None:
+        self.first_barrier.wait()
+        # printSecond() outputs "second". Do not change or remove this line.
+        printSecond()
+        self.second_barrier.wait()
+
+
+    def third(self, printThird: 'Callable[[], None]') -> None:
+        self.second_barrier.wait()
+        # printThird() outputs "third". Do not change or remove this line.
+        printThird()
