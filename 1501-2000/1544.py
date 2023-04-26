1d16b4d54feb9cd168bf97e5d413aff36c8945ed
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2023-04-26 19:01:44 +0530

    Update 1544.py

--- 1501-2000/1544.py
+++ 1501-2000/1544.py
@@ -2,6 +2,21 @@ class Solution:
     def makeGood(self, s: str) -> str:
         stk = []
         for c in s:
+            if stk:
+                if abs(ord(stk[-1])-ord(c))==32:
+                    stk.pop()
+                else:
+                    stk.append(c)
+            else:
+                stk.append(c)
+
+        return "".join(stk)
+
+
+class Solution:
+    def makeGood(self, s: str) -> str:
+        stk = []
+        for c in s:
             if c.isupper() and stk and stk[-1]==c.lower():
                 stk.pop()
             elif c.islower() and stk and stk[-1]==c.upper():
