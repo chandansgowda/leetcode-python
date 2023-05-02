74d82e81312fee304e173c42d9c322fe1e2009f6
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2023-05-02 19:43:52 +0530

    Update 1822.py

--- 1501-2000/1822.py
+++ 1501-2000/1822.py
@@ -1,5 +1,14 @@
 class Solution:
     def arraySign(self, nums: List[int]) -> int:
+        c = 0
+        for num in nums:
+            if num==0: return 0
+            elif num<0: c+=1
+        return 1 if not c%2 else -1
+
+
+class Solution:
+    def arraySign(self, nums: List[int]) -> int:
         p = 1
         for num in nums:
             p *= num
