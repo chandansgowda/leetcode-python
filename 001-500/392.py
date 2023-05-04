13fb9a67894a70dcc983c1d1a29ae47542d85b1c
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2023-05-04 19:41:53 +0530

    Create 392.py

---
+++ 001-500/392.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        if not s:
+            return True
+        i = 0
+        for c in t:
+            if s[i]==c:
+                i+=1
+            if i==len(s):
+                break
+
+        return i==len(s)
