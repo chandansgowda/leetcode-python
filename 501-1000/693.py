c1597525f1f3f82d575fa68b9ee18b071d695447
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2023-05-13 22:00:36 +0530

    Update 693.py

--- 501-1000/693.py
+++ 501-1000/693.py
@@ -9,3 +9,9 @@ class Solution:
             else:
                 return False
         return True
+
+    
+class Solution:
+    def hasAlternatingBits(self, n: int) -> bool:
+        bits = bin(n)
+        return "00" not in bits and "11" not in bits
