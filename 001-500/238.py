6e52993d4650a08adc8561d4b3b2e4225a24c37f
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2023-05-25 18:24:11 +0530

    Update 238.py

--- 001-500/238.py
+++ 001-500/238.py
@@ -1,6 +1,24 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         res = [1]*len(nums)
+
+        pre = 1
+        for i in range(len(nums)):
+            res[i] = pre
+            pre *= nums[i]
+
+        post = 1
+        for i in reversed(range(len(nums))):
+            res[i] *= post
+            post *= nums[i]
+
+        return res
+
+
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        res = [1]*len(nums)
         pre = 1
         post = 1
 
