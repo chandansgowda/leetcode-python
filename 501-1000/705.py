d5f106650af3b937bb5b98ffcd21988ed1e0a760
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2023-05-30 20:55:19 +0530

    Create 705.py

---
+++ 501-1000/705.py
@@ -0,0 +1,21 @@
+class MyHashSet:
+
+    def __init__(self):
+        self.hashset = set()
+
+    def add(self, key: int) -> None:
+        self.hashset.add(key)
+
+    def remove(self, key: int) -> None:
+        if key in self.hashset:
+            self.hashset.remove(key)
+
+    def contains(self, key: int) -> bool:
+        return key in self.hashset
+
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
