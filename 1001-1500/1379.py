6b790c6ee508668f029b4461f629370243b68135
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2023-06-03 19:34:49 +0530

    Create 1379.py

---
+++ 1001-1500/1379.py
@@ -0,0 +1,15 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def getTargetCopy(self, original: TreeNode, cloned: TreeNode, target: TreeNode) -> TreeNode:
+        if not original:
+            return None
+        if original is target:
+            return cloned
+        return self.getTargetCopy(original.left,cloned.left,target) or self.getTargetCopy(original.right,cloned.right,target)
+
