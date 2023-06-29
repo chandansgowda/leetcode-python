c9bc796e9c41a16d627137417c7befa389378868
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2023-06-29 21:42:56 +0530

    Update 015.py

--- 001-500/015.py
+++ 001-500/015.py
@@ -1,5 +1,45 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        n,p,z = [],[],[]
+        res = set()
+
+        for num in nums:
+            if num==0:
+                z.append(num)
+            elif num>0:
+                p.append(num)
+            else:
+                n.append(num)
+
+        N,P = set(n), set(p)
+
+        if z:
+            for num in P:
+                if -num in N:
+                    res.add((num,-num,0))
+
+            if len(z)>=3:
+                res.add((0,0,0))
+
+        for i in range(len(p)):
+            for j in range(i+1,len(p)):
+                target = -(p[i]+p[j])
+                if target in N:
+                    res.add(tuple(sorted([target, p[i], p[j]])))
+
+        for i in range(len(n)):
+            for j in range(i+1,len(n)):
+                target = -(n[i]+n[j])
+                if target in P:
+                    res.add(tuple(sorted([target, n[i], n[j]])))
+
+        return res
+
+
+
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
         ans = []
 
