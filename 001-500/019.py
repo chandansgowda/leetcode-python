fc71a98f4499bb1e11f2b08c50bee2a2ff0a2445
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2023-07-07 21:35:32 +0530

    Update 019.py

--- 001-500/019.py
+++ 001-500/019.py
@@ -43,3 +43,23 @@ class Solution:
             slow = slow.next
         slow.next = slow.next.next
         return head
+
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+
+        t = head
+        nodes = 0
+
+        while t:
+            nodes+=1
+            t = t.next
+        
+        dummy = ListNode()
+        dummy.next = head
+        t = dummy
+        p = None
+        for i in range(nodes-n+1):
+            p = t
+            t = t.next
+        p.next=t.next
+        return dummy.next
