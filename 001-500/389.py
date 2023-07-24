f7af3c60880006ddaeede071bbdbf7fac7b532f6
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2023-07-24 21:24:08 +0530

    Update 389.py

--- 001-500/389.py
+++ 001-500/389.py
@@ -8,3 +8,14 @@ class Solution:
             y += ord(c)
 
         return chr(abs(x-y))
+
+
+
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        x = 0
+        for c in s:
+            x ^= ord(c)
+        for c in t:
+            x ^= ord(c)
+        return chr(x)
