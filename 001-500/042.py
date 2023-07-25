ab18b88ae3eeddc36e612a3b92f5ddf7e914ba2e
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2023-07-25 17:46:41 +0530

    Update 042.py

--- 001-500/042.py
+++ 001-500/042.py
@@ -1,3 +1,29 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        water = 0
+        maxLeftArr = [0]*len(height)
+        maxRightArr = [0]*len(height)
+
+        maxLeft = height[0]
+        for i in range(1,len(height)):
+            maxLeftArr[i] = maxLeft
+            if height[i]>maxLeft:
+                maxLeft=height[i]
+
+        maxRight = height[-1]
+        for i in reversed(range(len(height)-1)):
+            maxRightArr[i] = maxRight
+            if height[i]>maxRight:
+                maxRight=height[i]
+
+        for i in range(1,len(height)-1):
+            bar_height = min(maxLeftArr[i], maxRightArr[i])
+            if bar_height>height[i]:
+                water += bar_height-height[i]
+        return water
+        
+
+
 # Exceeds Time Limit
 class Solution:
     def trap(self, height: List[int]) -> int:
