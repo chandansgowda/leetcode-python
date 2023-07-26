9e038387f73f5fd794175dfa6be9a5fa32952a64
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2023-07-26 22:18:36 +0530

    Update 118.py

--- 001-500/118.py
+++ 001-500/118.py
@@ -7,3 +7,11 @@ class Solution:
                 res[i][j]=res[i-1][j]+res[i-1][j-1]
 
         return res
+
+
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        res = [[1]]
+        for _ in range(numRows):
+            res.append(list(map(add, res[-1]+[0], [0]+res[-1])))
+        return res[:numRows]
