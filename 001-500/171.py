cc387b82bcf794940ec43fd212cd325e026ff66b
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2023-07-27 22:17:42 +0530

    Update 171.py

--- 001-500/171.py
+++ 001-500/171.py
@@ -4,3 +4,12 @@ class Solution:
         for i,c in enumerate(reversed(columnTitle)):
             res += (26**i) * (ord(c) - ord('A') + 1)
         return res
+
+
+class Solution:
+    def titleToNumber(self, columnTitle: str) -> int:
+        res,count = 0,len(columnTitle)-1
+        for c in columnTitle:
+            res += (26**count) * (ord(c) - ord('A') + 1)
+            count-=1
+        return res
