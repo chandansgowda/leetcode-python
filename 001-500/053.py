89c3ab9de02de94beba160ebcedc1aa9d5b73560
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2023-08-02 19:26:02 +0530

    Update 053.py

--- 001-500/053.py
+++ 001-500/053.py
@@ -4,3 +4,15 @@ class Solution:
         for idx,num in enumerate(nums):
             dp[idx] = max(dp[idx-1]+num, num)
         return max(dp)
+
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        curSum = 0
+        maxSum = nums[0]
+        for num in nums:
+            if curSum<0:
+                curSum = 0
+            curSum+=num
+            maxSum = max(curSum,maxSum)
+        return maxSum
