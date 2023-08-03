e0bd76a3e18fdef2ec316305561da4341653276a
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2023-08-03 21:04:02 +0530

    Create 200.py

---
+++ 001-500/200.py
@@ -0,0 +1,33 @@
+from collections import deque
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
+            return 0
+        rows,cols = len(grid),len(grid[0])
+        visited = set()
+        islands = 0
+
+        def bfs(r,c):
+            q = deque()
+            q.append((r,c))
+            visited.add((r,c))
+            while q:
+                row,col = q.popleft()
+                directions = [[0,1],[1,0],[-1,0],[0,-1]]
+                for dr,dc in directions:
+                    r,c=row+dr,col+dc
+                    if (r in range(rows) and
+                    c in range(cols) and
+                    grid[r][c]=="1" and
+                    (r,c) not in visited):
+                        q.append((r,c))
+                        visited.add((r,c))
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c]=="1" and (r,c) not in visited:
+                    bfs(r,c)
+                    islands += 1
+
+        return islands
