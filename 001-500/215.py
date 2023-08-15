74b25295dae4d7fb8c875a253d176731c4c3773e
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2023-08-15 23:36:32 +0530

    Update 215.py

--- 001-500/215.py
+++ 001-500/215.py
@@ -2,5 +2,16 @@ from heapq import *
 
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
+        h = []
+        res = 0
+        for num in nums:
+            heappush(h,num)
+            if len(h)>k:
+                heappop(h)
+        return heappop(h)
+
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
         heapify(nums)
         return nlargest(k,nums)[-1]
