667501b302f808068a335d56938251b1700abab9
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2023-08-29 20:43:18 +0530

    Update 322.py

--- 001-500/322.py
+++ 001-500/322.py
@@ -1,5 +1,28 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
+        def helper(i,amt,dp):
+            if i==0:
+                if amt%coins[0]==0:
+                    return amt//coins[0]
+                else:
+                    return float('inf')
+            if dp[i][amt]!=-1:
+                return dp[i][amt]
+            not_pick = helper(i-1,amt,dp)
+            if coins[i]<=amt:
+                pick = 1 + helper(i,amt-coins[i],dp)
+            else:
+                pick = float('inf')
+            dp[i][amt] = min(pick,not_pick)
+            return dp[i][amt]
+
+        dp = [[-1]*(amount+1) for _ in range(len(coins))]
+        res = helper(len(coins)-1, amount, dp)
+        return res if res!=float('inf') else -1
+
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
         def helper(i,amt):
             if i==0:
                 if amt%coins[0]==0:
