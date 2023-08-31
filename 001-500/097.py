e49f26458806924d1ab4d6a9c8a9fb3dd37ae0b3
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2023-08-31 21:48:01 +0530

    Update 097.py

--- 001-500/097.py
+++ 001-500/097.py
@@ -3,6 +3,35 @@ class Solution:
         l1,l2,l3 = len(s1),len(s2),len(s3)
         if l1+l2!=l3:
             return False
+
+        dp = [[-1]*(l2+1) for _ in range(l1+1)]
+        
+        def helper(i,j,k):
+            if k==l3:
+                return True
+            
+            if dp[i][j]!=-1:
+                return dp[i][j]
+            
+            ans = False
+            if i<l1 and s1[i]==s3[k]:
+                ans = ans or helper(i+1,j,k+1)
+            
+            if j<l2 and s2[j]==s3[k]:
+                ans = ans or helper(i,j+1,k+1)
+            
+            dp[i][j] = ans
+            return ans
+
+        return helper(0,0,0)
+        
+
+
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        l1,l2,l3 = len(s1),len(s2),len(s3)
+        if l1+l2!=l3:
+            return False
         
         def helper(i,j,k):
             if k==l3:
