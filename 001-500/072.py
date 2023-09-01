37e9b80e77c7d1054f8bb87c64349c041535b8c2
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2023-09-01 21:36:32 +0530

    Update 072.py

--- 001-500/072.py
+++ 001-500/072.py
@@ -1,5 +1,28 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
+        def helper(i,j,dp):
+            if i==0:
+                return j
+            if j==0:
+                return i
+            if dp[i][j]!=-1:
+                return dp[i][j]
+            if word1[i-1]==word2[j-1]:
+                dp[i][j] = helper(i-1,j-1,dp)
+            else:
+                insertOp = helper(i-1,j,dp)
+                deleteOp = helper(i,j-1,dp)
+                swapOp = helper(i-1,j-1,dp)
+                dp[i][j] = min(insertOp,deleteOp,swapOp) + 1
+            return dp[i][j]
+        
+        dp = [[-1]*(len(word2)+1) for _ in range(len(word1)+1)]
+        return helper(len(word1),len(word2),dp)
+
+
+
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
         def helper(i,j):
             if i==0:
                 return j
