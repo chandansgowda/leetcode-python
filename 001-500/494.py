5ff237fe00c36d8a47f6a64ec70be3f6c703521e
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2023-09-02 21:23:20 +0530

    Update 494.py

--- 001-500/494.py
+++ 001-500/494.py
@@ -1,5 +1,25 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        memo = {}
+
+        def helper(i, t):
+            if i==-1:
+                if t==0:
+                    return 1
+                return 0
+            if (i,t) in memo:
+                return memo.get((i,t))
+            pos = helper(i-1,t-nums[i])
+            neg = helper(i-1,t+nums[i])
+            memo[(i,t)] = pos+neg
+            return memo[(i,t)]
+
+        return helper(len(nums)-1,target)
+
+
+
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
         def helper(i, t):
             if i==-1:
                 if t==0:
