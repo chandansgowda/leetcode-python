8fb708d8403fd4cc204b87b27faa90ffaa942a40
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2023-09-06 18:49:53 +0530

    Update 2824.py

--- 2501-3000/2824.py
+++ 2501-3000/2824.py
@@ -1,5 +1,20 @@
 class Solution:
     def countPairs(self, nums: List[int], target: int) -> int:
+        nums.sort()
+        count = 0
+        left = 0
+        right = len(nums)-1
+        while left<right:
+            if (nums[left]+nums[right]<target):
+                count+= right-left
+                left += 1
+            else:
+                right -= 1
+        return count
+        
+
+class Solution:
+    def countPairs(self, nums: List[int], target: int) -> int:
         count = 0
         for i in range(len(nums)):
             for j in range(i+1,len(nums)):
