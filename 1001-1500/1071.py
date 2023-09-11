d11eef4ab52abac63032c5844a18bf8479339efd
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2023-09-11 20:53:29 +0530

    Create 1071.py

---
+++ 1001-1500/1071.py
@@ -0,0 +1,9 @@
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        if str1 + str2 != str2 + str1:
+            return ""
+            
+        max_length = gcd(len(str1), len(str2))
+        return str1[:max_length]
+
+        
