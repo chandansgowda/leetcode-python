192e059c6e10d889da9036a7fcfc118be66ba75c
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2023-10-26 21:42:23 +0530

    Update 062.py

--- 001-500/062.py
+++ 001-500/062.py
@@ -1,5 +1,19 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
+        dp = [[0]*m for _ in range(n)]
+        for i in range(m):
+            dp[0][i]=1
+        for j in range(n):
+            dp[j][0]=1
+
+        for i in range(1,n):
+            for j in range(1,m):
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+                
+        return dp[n-1][m-1]
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
         def helper(i,j,dp):
             if i==0 and j==0:
                 return 1
