16a7c4b9c9d0b88ab013899abfd504f3dc31bc25
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2023-10-26 21:33:20 +0530

    Create 062.py

---
+++ 001-500/062.py
@@ -0,0 +1,16 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        def helper(i,j,dp):
+            if i==0 and j==0:
+                return 1
+            elif i<0 or j<0:
+                return 0
+            elif dp[i][j]!=-1:
+                return dp[i][j]
+            l = helper(i-1,j,dp)
+            r = helper(i,j-1,dp)
+            dp[i][j] = l+r
+            return dp[i][j]
+        
+        dp = [[-1]*m for _ in range(n)]
+        return helper(n-1,m-1,dp)
