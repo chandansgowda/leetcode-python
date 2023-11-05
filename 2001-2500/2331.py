37ae63c0271c6b2ec49678aa5dda395c312b7d56
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2023-11-05 19:48:06 +0530

    Create 2331.py

---
+++ 2001-2500/2331.py
@@ -0,0 +1,15 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def evaluateTree(self, root: Optional[TreeNode]) -> bool:
+        if root.val==0 or root.val==1:
+            return root.val
+        elif root.val==2:
+            return self.evaluateTree(root.left) or self.evaluateTree(root.right)
+        elif root.val==3:
+            return self.evaluateTree(root.left) and self.evaluateTree(root.right)
+        
