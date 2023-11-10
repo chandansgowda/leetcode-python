0dd8a5686b73f925981b3f5e552f7142a890e06b
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2023-11-10 20:18:19 +0530

    Update 1464.py

--- 1001-1500/1464.py
+++ 1001-1500/1464.py
@@ -1,5 +1,12 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
+        maxval = max(nums)
+        nums.remove(maxval)
+        return (maxval-1)*(max(nums)-1)
+
+
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
         ans = 0
         for i in range(len(nums)):
             for j in range(i+1,len(nums)):
