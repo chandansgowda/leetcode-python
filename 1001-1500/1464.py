fb9cd31a5e464ed436f6e4ef669c5bc25d2ad4cd
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2023-11-10 20:18:03 +0530

    Create 1464.py

---
+++ 1001-1500/1464.py
@@ -0,0 +1,7 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        ans = 0
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                ans = max(ans, (nums[i]-1)*(nums[j]-1))
+        return ans
