b435ace2601ad3099f62a346584d6beca4197171
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2023-11-21 20:10:52 +0530

    Create 589.py

---
+++ 501-1000/589.py
@@ -0,0 +1,21 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def preorder(self, root: 'Node') -> List[int]:
+        def dfs(root,res):
+            if not root:
+                return
+            res.append(root.val)
+            for child in root.children:
+                dfs(child, res)
+        res = []
+        dfs(root, res)
+        return res
+    
+        
