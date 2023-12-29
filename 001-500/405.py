ebf7aa8a4d54bbaa2a6efced7172d6c7c10f8dfd
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2023-12-29 17:58:46 +0530

    Update 405.py

--- 001-500/405.py
+++ 001-500/405.py
@@ -9,4 +9,14 @@ class Solution:
             ans = c + ans
             num = num >> 4
         return ans.lstrip('0')
+
+
+class Solution:
+    def toHex(self, num: int) -> str:
+        s, res, num = '0123456789abcdef', '', num & 0xFFFFFFFF        
+        while num:
+            res += s[num % 16]
+            num >>= 4
+        return res[::-1] or '0'
+        
         
