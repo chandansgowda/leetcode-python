d82c231badb586a9f8dccda32e1e3824a3342fec
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2023-12-30 18:53:12 +0530

    Update 2859.py

--- 2501-3000/2859.py
+++ 2501-3000/2859.py
@@ -5,3 +5,8 @@ class Solution:
             if str(bin(i)).count("1")==k:
                 res+=nums[i]
         return res
+
+
+class Solution:
+    def sumIndicesWithKSetBits(self, nums: List[int], k: int) -> int:
+        return sum([nums[i] for i in range(len(nums)) if bin(i)[2:].count('1')==k])
