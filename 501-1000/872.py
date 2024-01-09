ecf3adaf3e52b8aa2d680d2a50da727522812727
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2024-01-09 21:32:59 +0530

    Update 872.py

--- 501-1000/872.py
+++ 501-1000/872.py
@@ -13,3 +13,16 @@ class Solution:
                 return [root.val]
             return findLeaf(root.left) + findLeaf(root.right)
         return findLeaf(root1)==findLeaf(root2)
+
+
+
+class Solution:
+    def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
+        def dfs(root):
+            if not root:
+                return []
+            if not root.left and not root.right:
+                return [root.val]
+            return dfs(root.left) + dfs(root.right)
+        return dfs(root2)==dfs(root1)
+        
