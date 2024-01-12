55de8a1c11fafba1b49ddd58613730dc32c4aeb8
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2024-01-12 14:27:00 +0530

    Update 1704.py

--- 1501-2000/1704.py
+++ 1501-2000/1704.py
@@ -10,3 +10,22 @@ class Solution:
                     count -= 1
 
         return count==0
+
+
+class Solution:
+    def halvesAreAlike(self, s: str) -> bool:
+        def is_vowel(x):
+            v = "aeiouAEIOU"
+            return x in v
+
+        c = 0
+        for i in range(len(s)//2):
+            if is_vowel(s[i]):
+                c+=1
+            if is_vowel(s[len(s)-i-1]):
+                c-=1
+            
+        return c==0
+
+
+        
