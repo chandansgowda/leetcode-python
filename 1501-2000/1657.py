864400499d0e707c50de304dff57c774f49f0293
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2024-01-14 21:30:59 +0530

    Create 1657.py

---
+++ 1501-2000/1657.py
@@ -0,0 +1,11 @@
+class Solution:
+    def closeStrings(self, word1: str, word2: str) -> bool:
+        if set(word1)!=set(word2):
+            return False
+        d1, d2 = defaultdict(int), defaultdict(int)
+        for c in word1:
+            d1[c]+=1
+        for c in word2:
+            d2[c]+=1
+        return sorted(d1.values())==sorted(d2.values())
+        
