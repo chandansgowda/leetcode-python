b8d4ee3cd7d826ed29f200885967334c9a4f2d8c
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2024-01-16 18:36:45 +0530

    Update 380.py

--- 001-500/380.py
+++ 001-500/380.py
@@ -26,7 +26,37 @@ class RandomizedSet:
     def getRandom(self) -> int:
         return random.choice(self.data)
 
+
+class RandomizedSet:
+
+    def __init__(self):
+        self.memory = {}
+        self.values = []
         
+
+    def insert(self, val: int) -> bool:
+        if val in self.memory:
+            return False
+        else:
+            self.values.append(val)
+            self.memory[val]=len(self.values)-1
+            return True
+
+        
+    def remove(self, val: int) -> bool:
+        if val not in self.memory:
+            return False
+        else:
+            val_pos = self.memory[val]
+            self.memory[self.values[-1]] = val_pos
+            self.values[val_pos], self.values[-1] = self.values[-1], self.values[val_pos]
+            self.values.pop()
+            del self.memory[val]
+            return True
+        
+
+    def getRandom(self) -> int:
+        return random.choice(self.values)
 
 
 # Your RandomizedSet object will be instantiated and called as such:
