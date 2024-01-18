f0a2e2b746c9daf080457ede69884368d57a6002
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2024-01-18 18:34:46 +0530

    Update 070.py

--- 001-500/070.py
+++ 001-500/070.py
@@ -4,3 +4,11 @@ class Solution:
         for i in range(2,n+2):
             fib.append(fib[i-1]+fib[i-2])
         return fib[n+1]
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        a,b=1,1
+        while n>0:
+            a,b = b,a+b
+            n-=1
+        return a
