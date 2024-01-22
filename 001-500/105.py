be93cdfaf0e2b7cd7c21ebe9b304b9969374be38
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2024-01-22 20:18:50 +0530

    Create 105.py

---
+++ 001-500/105.py
@@ -0,0 +1,15 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        if not inorder:
+            return None
+        root = TreeNode(preorder[0])
+        mid = inorder.index(root.val)
+        root.left = self.buildTree(preorder[1:mid+1], inorder[:mid])
+        root.right = self.buildTree(preorder[mid+1:], inorder[mid+1:])
+        return root
