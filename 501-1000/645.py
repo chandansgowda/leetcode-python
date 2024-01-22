313a75b8d33a5fc349b20992e8b427311b04a420
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2024-01-22 20:18:04 +0530

    Update 645.py

--- 501-1000/645.py
+++ 501-1000/645.py
@@ -1,4 +1,10 @@
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
         return [sum(nums)-sum(set(nums)), int(len(nums)*(len(nums)+1)/2-sum(set(nums)))]
+
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        x = (len(nums)*(len(nums)+1))//2
+        s = sum(set(nums))
+        return [sum(nums)-s, x-s]
         
