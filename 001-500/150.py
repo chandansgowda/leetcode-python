cdf352627dcf7df573901cdd85c6dddf4abef7a6
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2024-01-30 22:41:00 +0530

    Update 150.py

--- 001-500/150.py
+++ 001-500/150.py
@@ -18,3 +18,24 @@ class Solution:
                 stack.append(c)
                 
         return stack[-1]
+
+
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        s = []
+        x = "+-*/"
+        for token in tokens:
+            if token not in x:
+                s.append(int(token))
+            else:
+                a = s.pop()
+                b = s.pop()
+                if token=="+":
+                    s.append(a+b)
+                elif token=="-":
+                    s.append(b-a)
+                elif token=="*":
+                    s.append(a*b)
+                else:
+                    s.append(int(b/a))
+        return s[0]
