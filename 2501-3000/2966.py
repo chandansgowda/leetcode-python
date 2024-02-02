40b93198428472b4131dc5eac325050d0d3fd055
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2024-02-02 14:22:55 +0530

    Update 2966.py

--- 2501-3000/2966.py
+++ 2501-3000/2966.py
@@ -15,3 +15,16 @@ class Solution:
             else:
                 return []
         return result
+
+
+class Solution:
+    def divideArray(self, nums: List[int], k: int) -> List[List[int]]:
+        nums.sort()
+        ans=[]
+        n=len(nums)//3
+        for i in range(n):
+            if nums[3*i+2]-nums[3*i]> k: 
+                return []
+            ans.append(nums[3*i:3*i+3])
+        return ans
+        
